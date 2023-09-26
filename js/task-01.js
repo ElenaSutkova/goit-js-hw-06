e74fd8ef73dc575b24c2cc4c34068197242d00fd
ElenaSutkova <134842923+ElenaSutkova@users.noreply.github.com>
2023-09-26 17:15:55 +0300

    js3

--- js/task-01.js
+++ js/task-01.js
@@ -1,13 +1,13 @@
-const variables = {
-    categories: document.getElementById('categories'),
-    categoriesItems: document.querySelectorAll('#categories .item')    
-}
+// const variables = {
+//     categories: document.getElementById('categories'),
+//     categoriesItems: document.querySelectorAll('#categories .item')    
+// }
 
-console.log(`Number of categories: ${variables.categories.children.length}`);
+// console.log(`Number of categories: ${variables.categories.children.length}`);
 
-variables.categoriesItems.forEach(elm => {
-    const elementsListItems = elm.querySelectorAll('ul li');
+// variables.categoriesItems.forEach(elm => {
+//     const elementsListItems = elm.querySelectorAll('ul li');
 
-    console.log('Category: ', elm.firstElementChild.textContent);
-    console.log('Elements: ', elementsListItems.length);   
-});
+//     console.log('Category: ', elm.firstElementChild.textContent);
+//     console.log('Elements: ', elementsListItems.length);   
+// });
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const container = document.getElementById('ingredients');
+
+const itemIngredient = ingredients.map(ingredient => {
+  const addList = document.createElement('li');
+  addList.classList.add('item');
+  addList.textContent = ingredient;
+  return addList;
+});
+
+container.append(...itemIngredient)
